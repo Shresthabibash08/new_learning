673349472cce7975b0ff6331827e0e060ddbcea4
Bibash <bibash8@gmail.com>
2025-02-11 10:59:37 +0545

    kei change gareko

--- first.py
+++ first.py
@@ -1,11 +1,3 @@
 print("HEllO")
 print("Hi")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print ("Bibash")
-=======
-print("Bibash dada")
->>>>>>> 9bd5f6353c0bf095d886bd19f97ae048c8fece15
-=======
->>>>>>> 58a5056e6297840caa38d703b160e0ab211df1c4
-print("new")
+print("Bibash Dada123")
